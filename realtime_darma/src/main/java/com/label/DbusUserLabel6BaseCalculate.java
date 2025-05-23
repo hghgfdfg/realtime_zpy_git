184645cf3bb2b12c4829a3466b843588235b33d6
Administrator <2955937194@qq.com>
2025-05-23 22:16:51 +0800

    Dharma add

--- realtime_darma/src/main/java/com/label/DbusUserLabel6BaseCalculate.java
+++ realtime_darma/src/main/java/com/label/DbusUserLabel6BaseCalculate.java
@@ -25,6 +25,7 @@ public class DbusUserLabel6BaseCalculate {
 
 //
 
+
     private static final String kafka_botstrap_servers = ConfigUtils.getString("kafka.bootstrap.servers");
     private static final String kafka_label_base6_topic = ConfigUtils.getString("kafka.result.label.base6.topic");
     private static final String kafka_label_base4_topic =    ConfigUtils.getString("kafka.result.label.base4.topic");
